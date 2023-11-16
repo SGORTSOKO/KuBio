c2d30ea8f6089168248450573176bf1bc11e4a31
abrosimov_vv <sgortsoko@yandex.ru>
2023-11-16 14:36:52 +0500

    1

--- app/src/main/java/ksu/sirius/kubio/MainActivity.kt
+++ app/src/main/java/ksu/sirius/kubio/MainActivity.kt
@@ -3,43 +3,49 @@ package ksu.sirius.kubio
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import ksu.sirius.kubio.ui.theme.KuBioTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            KuBioTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("MainActivity")
-                }
-            }
+            MainWidget()
         }
     }
-}
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+
 }
 
-@Preview(showBackground = true)
+@Preview
 @Composable
-fun GreetingPreview() {
-    KuBioTheme {
-        Greeting("Preview")
+fun MainWidget(){
+    ListItem("Vlad", "Programmer")
+}
+@Composable
+fun ListItem(name: String,profession: String){
+    Card(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(5.dp)
+    ){
+        Box(
+        ){
+            Row(){
+                Image(painter = painterResource(id = R.drawable.ic_launcher_foreground), contentDescription = "Android")
+            }
+        }
     }
 }
