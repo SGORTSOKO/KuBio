7ef8a2f8ea7033b3e693e7f47644a0d7bd9d24a2
abrosimov_vv <sgortsoko@yandex.ru>
2023-11-29 09:22:08 +0500

    train class enum

--- app/src/main/java/ksu/sirius/kubio/ui/theme/TrainPacketOOP.kt
+++ app/src/main/java/ksu/sirius/kubio/ui/theme/TrainPacketOOP.kt
@@ -2,12 +2,65 @@ package ksu.sirius.kubio.ui.theme
 
 import android.util.Log
 
-open class BaseTrainClass(open var newInnerFieldValue: String = "") {
+interface BaseTrainInterface{
+    fun Go() : Int
+}
+
+interface TrainInterfaceFirst: BaseTrainInterface{
+    override fun Go(): Int {
+        return 0
+    }
+}
+
+interface TrainInterfaceSecond: BaseTrainInterface{
+    override fun Go(): Int {
+        return 1
+    }
+}
+
+interface TrainInterfaced: TrainInterfaceFirst, TrainInterfaceSecond{
+    override fun Go(): Int {
+        var numbers = super<TrainInterfaceFirst>.Go()
+        numbers += super<TrainInterfaceSecond>.Go()
+        return numbers
+    }
+}
+
+enum class BaseTrainEnumInt(val value: Int) {
+    Monday(1),
+    Tuesday(2),
+    Wednesday(3),
+    Thursday(4),
+    Friday(5),
+    Saturday(6),
+    Sunday(7);
+
+    fun getDuration(day: BaseTrainEnumInt): Int{
+        return value - day.value;
+    }
+}
+
+enum class BaseTrainEnumString(val value: String) {
+    Monday("Monday"),
+    Tuesday("Tuesday"),
+    Wednesday("Wednesday"),
+    Thursday("Thursday"),
+    Friday("Friday"),
+    Saturday("Saturday"),
+    Sunday("Sunday");
+}
+
+
+
+
+
+
+abstract class BaseTrainClass(innerStringField: String = ".") {
     protected open fun Errorer(message: String = ""){
 
         Log.d("BaseTrainClass", message)
     }
-    public open var innerStringField: String = ""
+    var innerStringField: String = ""
         set(newValue){
             when(newValue.length){
                 0 -> return
@@ -19,15 +72,8 @@ open class BaseTrainClass(open var newInnerFieldValue: String = "") {
         get(){
             return "Value: $field"
         }
-    init{
-        innerStringField = newInnerFieldValue
-    }
 }
 
-open class BaseTrainClassOverride(override var newInnerFieldValue: String = "_"):
-    BaseTrainClass(newInnerFieldValue){
-
-    init{
-        innerStringField = newInnerFieldValue
-    }
+final class BaseTrainClassOverride(newInnerFieldValue: String = "_"):
+    BaseTrainClass(newInnerFieldValue), TrainInterfaced{
 }
